dff6a8cdf82dd6384eecfe77dcab6d2d106a4299
yuk <yukarinrin@163.com>
2018-10-24 20:19:50 +0800

    1

--- src/app/layout/normal/header/header.component.ts
+++ src/app/layout/normal/header/header.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BreakpointObserver } from '@angular/cdk/layout'
+import { Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  findName: string;
+  constructor(
+    private breakpointObserver: BreakpointObserver
+  ) { }
 
   ngOnInit() {
+    const layoutChanges = this.breakpointObserver.observe([
+      '(max-width: 960px)',
+
+    ]);
+
+    layoutChanges.subscribe(result => {
+      if(result.matches) {
+        this.findName = 'FIND A PRO';
+      } else {
+        this.findName = 'Find a Pool Pro';
+      }
+    })
   }
 
 }
